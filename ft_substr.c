b49252db6ed7b964516e3d9a2f1644a695cba067
MRK <markhrynevich@gmail.com>
2024-03-09 16:14:35 +0100

    dfs

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: mrk <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 15:53:07 by mrk               #+#    #+#             */
-/*   Updated: 2024/03/09 16:13:06 by mrk              ###   ########.fr       */
+/*   Updated: 2024/03/09 16:14:30 by mrk              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -19,23 +19,19 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 	i = 0;
 	count = start;
-	if (start <= ft_strlen((char *)s))
-        {
-		while (s[start] && start < len)
-			start++;
-		sub = (char *)malloc(sizeof(char) * (start + 1));
-		if (!sub)
-			return (NULL);
-		while (i < start + 1 && i < len && count < ft_strlen((char *)s))
-		{
-			sub[i] = s[count];
-			i++;
-			count++;
-		}
-		sub[i] = '\0';
-	return (sub);
+	while (s[start] && start < len)
+		start++;
+	sub = (char *)malloc(sizeof(char) * (start + 1));
+	if (!sub)
+		return (NULL);
+	while (i < start + 1 && i < len && count < ft_strlen((char *)s))
+	{
+		sub[i] = s[count];
+		i++;
+		count++;
 	}
-	return (NULL);
+	sub[i] = '\0';
+	return (sub);
 }
 /*#include <stdio.h>
 int	main()
