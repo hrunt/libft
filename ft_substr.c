3961616b8b00040f10931cc281b71436b4c2035a
MRK <markhrynevich@gmail.com>
2024-03-11 14:53:19 +0100

    sadsa

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: mrk <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 15:53:07 by mrk               #+#    #+#             */
-/*   Updated: 2024/03/11 14:48:44 by mrk              ###   ########.fr       */
+/*   Updated: 2024/03/11 14:53:13 by mrk              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -30,7 +30,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	sub = (char *)malloc(sizeof(char) * (count + 1));
 	if (!sub)
 		return (NULL);
-	while (i < start + 1 && i < len && start < ft_strlen((char *)s))
+	while (i < count + 1 && i < len && start < ft_strlen((char *)s))
 	{
 		sub[i] = s[count];
 		i++;
@@ -39,10 +39,10 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	sub[i] = '\0';
 	return (sub);
 }
-#include <stdio.h>
+/*#include <stdio.h>
 int	main()
 {
 	char s1[] = "0123456789";
 	char *s2 = ft_substr(s1, 2, 1);
 	printf("%s", s2); 
-}
+}*/
