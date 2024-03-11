f74ae294ff8c3575c7463933fdfe468855788fc8
MRK <markhrynevich@gmail.com>
2024-03-11 15:26:24 +0100

    sadsa

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: mrk <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 15:53:07 by mrk               #+#    #+#             */
-/*   Updated: 2024/03/11 15:18:10 by mrk              ###   ########.fr       */
+/*   Updated: 2024/03/11 15:26:15 by mrk              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -19,15 +19,17 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 	i = 0;
 	count = 0;
+	if (s == NULL)
+		return (NULL);
 	if (start >= ft_strlen((char *)s) || len == 0)
 		count = 0;
 	else
 	{
-		while (s[start] && count < len)
+		while (s[start] && count < len - 1)
 			count++;
 			start++;
 	}
-	sub = (char *)malloc(sizeof(char) * (count + 1));
+	sub = malloc(sizeof(char) * (count + 1));
 	if (!sub)
 		return (NULL);
 	while (i < count + 1 && i < len)
