508f79b6ee0b851670b05cd724493081599576fc
MRK <markhrynevich@gmail.com>
2024-03-11 16:05:26 +0100

    sadsa

--- ft_substr.c
+++ ft_substr.c
@@ -6,47 +6,41 @@
 /*   By: mrk <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 15:53:07 by mrk               #+#    #+#             */
-/*   Updated: 2024/03/11 15:50:01 by mrk              ###   ########.fr       */
+/*   Updated: 2024/03/11 16:05:15 by mrk              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	unsigned int	i;
 	unsigned int	p;
 	unsigned int	c;
 	char			*sub;
 
-	i = 0;
 	c = 0;
 	p = start;
-	if (s == NULL)
+	if (!s)
 		return (NULL);
-	if (start >= ft_strlen((char *)s) || len == 0)
-		return (malloc(1));
-	while (s[p] && i < len)
-	{
-		p++;
-		i++;
-	}
-	sub = malloc(sizeof(char) * (i + 1));
+	if (start > ft_strlen((char *)s))
+		len = 0;
+	else if (len > (ft_strlen((char *)s) - start))
+		len = ft_strlen((char *)s) - start;
+	sub = (char *)malloc(sizeof(char) * (len + 1));
 	if (!sub)
 		return (NULL);
-	p = start;
-	while (c < i && i < len)
+	while (*s && len--)
 	{
 		sub[c] = s[p];
 		c++;
 		p++;
 	}
-	sub[i] = '\0';
+	sub[c] = '\0';
 	return (sub);
 }
 /*#include <stdio.h>
 int	main()
 {
 	char s1[] = "0123456789";
-	char *s2 = ft_substr(s1, 45, 5);
+	char *s2 = ft_substr(s1, 2, 15);
 	printf("%s", s2); 
 }*/
