5ca53714c8c2b3c80e91afc96ccb727298f5cc24
MRK <markhrynevich@gmail.com>
2024-03-11 17:50:26 +0100

    ghgfh

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: mrk <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 14:35:20 by mrk               #+#    #+#             */
-/*   Updated: 2024/03/11 17:47:04 by mrk              ###   ########.fr       */
+/*   Updated: 2024/03/11 17:50:02 by mrk              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -60,7 +60,7 @@ char	**ft_split(char const *s, char c)
 	int		i;
 	int		m;
 
-	if (!s)
+	if (!s || count((char *)s, c) == 0)
 		return (NULL);
 	ar = malloc (sizeof(char *) * (count((char *)s, c) + 1));
 	if (!ar)
