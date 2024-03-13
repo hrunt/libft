5269ad52df9444ff7efb8909f50a760d772fbf19
MRK <markhrynevich@gmail.com>
2024-03-13 14:02:01 +0100

    dfsdf

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,10 +6,9 @@
 /*   By: mrk <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 18:43:20 by mrk               #+#    #+#             */
-/*   Updated: 2024/03/04 16:48:56 by mrk              ###   ########.fr       */
+/*   Updated: 2024/03/13 13:59:43 by mrk              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <stddef.h>
 #include "libft.h"
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
@@ -20,9 +19,11 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 
 	bg = 0;
 	lt = 0;
-	length = ft_strlen((char *) little);
-	if (ft_strlen((char *)little) == 0)
+	if (!big && !len)
+		return (NULL);
+	if (!*little)
 		return ((char *)big);
+	length = ft_strlen((char *) little);
 	while (big[bg] && (bg + length) <= len)
 	{
 		if (big[bg] == little[lt] && ft_strncmp(big + bg, little, length) == 0)
@@ -34,7 +35,7 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 /*#include <stdio.h>
 int	main(void)
 {
-	char *big = "lorem ipsum dolor sit amet";
+	char big[0];
 	char little[] ="dolor";
-	printf("%s\n", ft_strnstr(big, little, 0));
+	printf("%s\n", ft_strnstr(NULL, "", 0));
 }*/
